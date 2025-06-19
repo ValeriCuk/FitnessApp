2d1d40e93d6c76708a9284b1f696a2b125928bdf
ValeriCuk <pakutasieva@gmail.com>
2025-06-19 17:22:41 +0300

    fill main method for checking print info method work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Athlete athlete = new Athlete("Ron", 25, 80);
+
+        Training runMonday = new Running(120, 35, athlete, 3);
+        Training runWednesday = new Running(130, 40, athlete, 2);
+        Training runFriday = new Running(115, 55, athlete, 5);
+
+        Training crossTuesday = new Crossfit(110, 60, athlete, 5);
+        Training crossThursday = new Crossfit(95, 60, athlete, 10);
+
+        Training boxingSaturday = new Boxing(130, 120, athlete);
+
+        List<Training> trainings = List.of(runMonday, runWednesday, runFriday, crossTuesday, crossThursday, boxingSaturday);
+
+        trainings.forEach(Training::printInfo);
     }
 }
